0b9abf88d37707d899fa6a50ce0b2c3ed1fc1d9b
Mauro Montecino <99820520+mauromontecino@users.noreply.github.com>
2022-10-18 09:30:08 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
-    draggable: true,
+    	draggable: true,
 		dots: '.carousel__indicadores',
 		arrows: {
 			prev: '.carousel__anterior',
@@ -12,14 +12,15 @@ window.addEventListener('load', function(){
 			{
 			  // screens greater than >= 775px
 			  breakpoint: 710,
+			  draggable: true,
 			  settings: {
-				draggable: true,
 				slidesToShow: 1,
 				slidesToScroll: 1
 			  }
 			},{
 			  // screens greater than >= 1024px
 			  breakpoint: 800,
+			  draggable: true,
 			  settings: {
 				slidesToShow: 2,
 				slidesToScroll: 2
@@ -29,12 +30,28 @@ window.addEventListener('load', function(){
       {
 			  // screens greater than >= 1024px
 			  breakpoint: 1310,
+			  draggable: true,
 			  settings: {
 				slidesToShow: 3,
 				slidesToScroll: 2
 			  }
 			}
 		]
+	});
+});
+
+
+
+window.addEventListener('load', function(){
+	new Glider(document.querySelector('.reviews__card'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+    	draggable: true,
+		dots: '.reviews__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente'
+		}
 	});
 });
 
